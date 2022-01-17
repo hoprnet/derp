99d49c891d89b09caae3614d895099c2064c7df8
Tino Breddin <tino@hoprnet.org>
2022-01-17 10:42:51 +0100

    Fix async build script

--- build.js
+++ build.js
@@ -5,19 +5,18 @@ import { build } from "esbuild";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-try {
-  await build({
-    bundle: true,
-    sourcemap: true,
-    format: "esm",
-    target: "esnext",
-    entryPoints: [path.join(__dirname, "src", "index.ts")],
-    outdir: path.join(__dirname, "dist"),
-    outExtension: { ".js": ".mjs" },
-    loader: {
-      ".html": "text",
-    },
-  });
-} catch {
+build({
+  bundle: true,
+  sourcemap: true,
+  format: "esm",
+  target: "esnext",
+  entryPoints: [path.join(__dirname, "src", "index.ts")],
+  outdir: path.join(__dirname, "dist"),
+  outExtension: { ".js": ".mjs" },
+  loader: {
+    ".html": "text",
+  },
+}).catch((error) => {
+  console.error(error);
   process.exitCode = 1;
-}
+});
