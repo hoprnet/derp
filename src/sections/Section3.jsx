7000ea682d844443e8ebdaadcf956badf4963fb2
Michal Jadach <michal.jadach@hoprnet.org>
2022-11-28 20:21:56 +0100

    moved rpch up

--- src/sections/Section3.jsx
+++ src/sections/Section3.jsx
@@ -14,6 +14,7 @@ function Section3() {
     return (
         <SSection
             id={'Section3'}
+            center
         >
             <Typography type="h2">
                 FURTHER EXAMPLES
