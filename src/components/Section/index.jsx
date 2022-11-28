c1cf83de6d06663e92cbc06f179c74c26db1c4a6
Michal Jadach <michal.jadach@hoprnet.org>
2022-11-28 13:25:32 +0100

    lightBlue for RPCh section

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -8,12 +8,28 @@ const SSection = styled.section`
   &.section--yellow{
     background: #FFFFA0;
   }
+  &.section--light-blue-gradient{
+    background: linear-gradient(180deg,#1ad1ff -110.52%,hsla(0,0%,85%,0) 105%);
+  }
+  &.section--light-blue{
+    background: #7ee5ff;
+  }
   &.section--dark-gradient{
     background: linear-gradient(180deg, #000050 0.5%, #0000B4 100%);
   }
   &.section--grey {
     background: #EEEEEE;
   }
+  &.full-height-min {
+    min-height: calc( 100vh - 68px - 170px);
+    @media (max-width: 850px) {
+      min-height: calc( 100vh - 68px - 294px);
+    }
+  }
+  &.full-height {
+    min-height: calc( 100vh - 60px);
+  }
+  
   padding-bottom: 40px;
   padding-top: 40px;
 `
@@ -34,8 +50,17 @@ const Content = styled.div`
 function Section(props) {
     return (
         <SSection
-            className={`Section ${props.className} ${props.gradient ? ' section--gradient' : ''}${props.yellow ? ' section--yellow' : ''}${props.darkGradient ? ' section--dark-gradient' : ''}${props.grey ? ' section--grey' : ''}`}
-            id={props.id}
+          className={`Section ${props.className} 
+            ${props.gradient ? ' section--gradient' : ''}
+            ${props.yellow ? ' section--yellow' : ''}
+            ${props.darkGradient ? ' section--dark-gradient' : ''}
+            ${props.lightBlueGradient ? ' section--light-blue-gradient' : ''}
+            ${props.lightBlue ? ' section--light-blue' : ''}
+            ${props.grey ? ' section--grey' : ''}
+            ${props.fullHeightMin ? ' full-height-min' : ''}
+            ${props.fullHeight ? ' full-height' : ''}`
+          }
+          id={props.id}
         >
             <Content className={`Content ${props.center ? ' content--center' : ''}`}>
                 {props.children}
