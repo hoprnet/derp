430ff11fae44a7b2c7af5b51f201873b55d97330
mjadach-iv <104129084+mjadach-iv@users.noreply.github.com>
2022-12-15 09:35:39 +0100

    map zoom logic (#24)
    * map zoom logic
    
    * unused vars remove

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Circle } from 'react-leaflet'
+import {useMapEvents} from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 
+
 const Map = (props) => {
+  const [zoomState, set_zoomState] = useState();
+  const [showMarker, set_showMarker] = useState(true);
+
+  function MyComponent(props) {
+    const [zoomLevel, setZoomLevel] = useState(null); // initial zoom level provided for MapContainer
+    const mapEvents = useMapEvents({
+        zoomend: () => {
+            setZoomLevel(mapEvents.getZoom());
+        },
+    });
+    if(zoomLevel){
+      props.updateZoom(zoomLevel);
+    }
+    return null
+  }
+
+  useEffect(() => {
+    if(zoomState >= 9) set_showMarker(false)
+    else set_showMarker(true)
+  }, [zoomState]);
+
+
   return (
     <div
       {...props}
@@ -21,9 +45,19 @@ const Map = (props) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker 
-          position={[props.coordinates.lat, props.coordinates.long]}
-      //     src={markerIcon}
+        {
+          showMarker && 
+          <Marker 
+            position={[props.coordinates.lat, props.coordinates.long]}
+          />
+        }
+        <Circle 
+          center={[props.coordinates.lat, props.coordinates.long]} 
+          radius={4000} 
+   //       pane="my-existing-pane" 
+        />
+        <MyComponent 
+          updateZoom={set_zoomState}
         />
       </MapContainer>
     </div>
