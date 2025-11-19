e85f64b539e5d7d21cb42110e669d3974f7a4c52
Tino Breddin <tino@hoprnet.org>
2025-11-19 15:00:08 +0100

    feat(nix): Add js/ts formatter

--- flake.nix
+++ flake.nix
@@ -1,14 +1,28 @@
 {
   description = "dishonest ethereum RPC provider";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "derp";
-      shell = ./shell.nix;
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.deno.enable = true;
+          settings.formatter.deno.includes = [ "*.ts" "*.js" ];
+        };
+      in
+      {
+        formatter = treefmtEval.config.build.wrapper;
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      }
+    );
 }
